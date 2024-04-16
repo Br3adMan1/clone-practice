64cd133cf78671a8637d0b617c9d89c3c2820759
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2024-04-16 12:42:12 +0300

    Created Class Word

--- src/test/Word.java
+++ src/test/Word.java
@@ -1,7 +1,46 @@
 package test;
-
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Word {
+    Tile[] tiles;
+    int row,col;
+    boolean vertical;
+    public Word(Tile[] tiles, int row, int col, boolean vertical){
+        this.tiles = Arrays.copyOf(tiles,tiles.length);
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
 
-	
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return row == word.row && col == word.col && vertical == word.vertical && Arrays.equals(tiles, word.tiles);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(row, col, vertical);
+        result = 31 * result + Arrays.hashCode(tiles);
+        return result;
+    }
 }
