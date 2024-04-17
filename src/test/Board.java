b59c8b1a80f17c747ffbbdee7081fabc06e07b94
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2024-04-17 14:13:43 +0300

    created a method that finds all the words created by adding a new word

--- src/test/Board.java
+++ src/test/Board.java
@@ -1,4 +1,5 @@
 package test;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Board {
@@ -45,6 +46,7 @@ public class Board {
     }
 
     boolean checkVertical(Word w){
+        int length = w.tiles.length;
         if(w.row + length - 1 > 15)
             return false;
         boolean isOnLetter = false;
@@ -55,12 +57,11 @@ public class Board {
                     return false;
             }
         }
-        if(!isOnLetter)
-            return false;
-        return  true;
+        return isOnLetter;
     }
 
     boolean checkHorizontal(Word w){
+        int length = w.tiles.length;
         if(w.col + length - 1 > 15)
             return false;
         boolean isOnLetter = false;
@@ -71,28 +72,22 @@ public class Board {
                     return false;
             }
         }
-        if(!isOnLetter)
-            return false;
-        return true;
+        return isOnLetter;
     }
 
-    public boolean containesMiddle(Word w){
+    public boolean containsMiddle(Word w){
         int length = w.tiles.length;
         if(w.vertical){
-            if(w.row<=7 && w.row + length - 1 >= 7)
-                return true;
+            return w.row <= 7 && w.row + length - 1 >= 7;
         }
         else{
-            if(w.col<=7 && w.col + length - 1 >= 7)
-                return true;
+            return w.col <= 7 && w.col + length - 1 >= 7;
         }
-        return false;
     }
     public boolean boardLegal(Word w){
-        int length = w.tiles.length;
         if(w.row < 0 || w.row > 15 || w.col < 0 || w.col > 15)
             return false;
-        if((!containesMiddle(w) && instance.tiles[7][7] == null))
+        if((!containsMiddle(w) && instance.tiles[7][7] == null))
             return false;
         if(w.vertical)
             return checkVertical(w);
@@ -100,6 +95,85 @@ public class Board {
     }
     boolean dictionaryLegal(Word w){
         return true; //temporary
+    }
+    private void addFirstWordToList(ArrayList<Word> list, Tile[] tiles, int row, int col, boolean vertical) {
+        Word temp = new Word(tiles, row, col, vertical);
+        for (int i = 0; i < tiles.length; i++) {
+            if (vertical) {
+                if (temp.tiles[i].letter == '_')
+                    temp.tiles[i] = instance.tiles[row + i][col];
+            } else {
+                if (temp.tiles[i].letter == '_')
+                    temp.tiles[i] = instance.tiles[row][col + i];
+            }
+        }
+        list.add(temp);
+    }
+
+    private int findBeginningOfWord(int row, int col, boolean vertical){
+        int temp;
+       if(vertical){
+           temp = row;
+           while(instance.tiles[temp-1][col] != null && temp > 0)
+               temp--;
+           return temp;
+       }
+       temp = col;
+       while(instance.tiles[row][temp-1]!=null && temp > 0)
+           temp--;
+       return temp;
+    }
+    private int findEndOfWord(int row, int col, boolean vertical){
+        int temp;
+        if(vertical){
+            temp = row;
+            while(instance.tiles[temp+1][col] != null && temp < 15)
+                temp++;
+            return temp;
+        }
+        temp = col;
+        while(instance.tiles[row][temp+1]!=null&& temp < 15)
+            temp++;
+        return temp;
+    }
+
+    private void findWord(Tile[] tempTiles,int beg, int size,Word w){
+        tempTiles = new Tile[size];
+        if(!w.vertical){
+            for (int i = 0; i< size; i++){
+                if(instance.tiles[beg+i][w.col] == null)
+                    tempTiles[i] = w.tiles[w.col-beg]; // insert the common tile with the original word
+                else{
+                    tempTiles[i] = instance.tiles[beg+i][w.col];
+                }
+            }
+        }
+        else{
+            for (int i = 0; i< size; i++){
+                if(instance.tiles[w.row][beg+i] == null)
+                    tempTiles[i] = w.tiles[w.row-beg]; // insert the common tile with the original word
+                else{
+                    tempTiles[i] = instance.tiles[w.row][beg+i];
+                }
+            }
+        }
+    }
+    public ArrayList<Word> getWords(Word w) {
+        ArrayList<Word> a = new ArrayList<>();
+        addFirstWordToList(a, w.tiles, w.row, w.col, w.vertical);
+        for (int i = 0; i< tiles.length; i++){
+            int beg = findBeginningOfWord(w.row,w.col,!w.vertical);
+            int end = findEndOfWord(w.row,w.col,!w.vertical);
+            int size = end - beg + 1;
+            Tile[] tempTiles = null;
+            findWord(tempTiles,beg,size,w);
+            if(w.vertical)
+                a.add(new Word(tempTiles,w.row,beg,!w.vertical));
+            else
+                a.add(new Word(tempTiles,beg,w.col,!w.vertical));
+        }
+
+        return a;
     }
 
 }
