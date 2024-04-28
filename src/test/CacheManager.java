dbf1871a4c169e6973b70e9e99a197e050786e97
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2024-04-28 18:01:15 +0300

    created Cache Manager

--- src/test/CacheManager.java
+++ src/test/CacheManager.java
@@ -1,8 +1,30 @@
 package test;
 
 
-public class CacheManager {
-	
-	
+import java.util.HashSet;
 
+public class CacheManager {
+    private int size;
+	private CacheReplacementPolicy crp;
+    private HashSet<String> words;
+
+    public CacheManager(int size, CacheReplacementPolicy crp) {
+        this.size = size;
+        this.crp = crp;
+        words = new HashSet<String>();
+    }
+
+    public boolean query(String s) {
+        return words.contains(s);
+    }
+
+
+    public void add(String s) {
+        if(this.words.size() == size) {
+            String toBeRemoved = crp.remove();
+            words.remove(toBeRemoved);
+        }
+        crp.add(s);
+        words.add(s);
+    }
 }
