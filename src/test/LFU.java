1faa85efed018ca95dc0efc2ac4254a747409641
Br3adMan1 <118209241+Br3adMan1@users.noreply.github.com>
2024-04-28 14:49:30 +0300

    implemented LRU and LFU

--- src/test/LFU.java
+++ src/test/LFU.java
@@ -1,6 +1,50 @@
 package test;
 
 
-public class LFU {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class LFU implements CacheReplacementPolicy {
+    private Map<String, Integer> frequencies;
+    private Map<String, Integer> order;
+    private int minFrequency;
+
+    public LFU() {
+        frequencies = new HashMap<>();
+        order = new LinkedHashMap<>();
+        minFrequency = 0;
+    }
+
+    @Override
+    public void add(String word) {
+        frequencies.put(word, frequencies.getOrDefault(word, 0) + 1);
+        order.put(word, order.size());
+
+        int wordFrequency = frequencies.get(word);
+        if (wordFrequency < minFrequency) {
+            minFrequency = wordFrequency;
+        }
+    }
+
+    @Override
+    public String remove() {
+        if (order.isEmpty()) {
+            return null;
+        }
+        String leastFrequentWord = null;
+        int minFreq = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Integer> entry : order.entrySet()) {
+            String word = entry.getKey();
+            int freq = frequencies.get(word);
+            if (freq <= minFreq) {
+                minFreq = freq;
+                leastFrequentWord = word;
+            }
+        }
+        frequencies.remove(leastFrequentWord);
+        order.remove(leastFrequentWord);
+        return leastFrequentWord;
+    }
 }
--- src/test/LRU.java
+++ src/test/LRU.java
@@ -1,6 +1,23 @@
 package test;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class LRU implements CacheReplacementPolicy {
+    private Deque<String> queue;
 
-public class LRU {
+    public LRU() {
+        queue = new ArrayDeque<>();
+    }
+    @Override
+    public void add(String word) {
+        if (queue.contains(word)) {
+            queue.remove(word);
+        }
+        queue.offerFirst(word);
+    }
 
+    @Override
+    public String remove() {
+        return queue.pollLast();
+    }
 }
